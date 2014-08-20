2a9a8556d15fda8e4f77c0654fc39c55a05bbc87
Geoff Hayes <hayesgm@gmail.com>
2014-08-20 16:25:16 -0700

    Making a good git chef source isn't trivial

--- libraries/install_plugin.rb
+++ libraries/install_plugin.rb
@@ -35,27 +35,49 @@ module Extensions
     
     if params['git']
       git_config = params['git']
-      ssh_key_file = nil
+      git_directory = "#{node[:elasticsearch][:home_dir]}/git"
+      plugin_directory = "#{git_directory}/#{name}"
 
-      # Allow SSH key connection to git
+      git_wrapper_file = "#{plugin_directory}/git_wrapper.sh"
+      ssh_key_file = nil
+      plugin_source_directory = "#{plugin_directory}/source"
+
+      # Create a directory for git and plugin
+      directory plugin_directory do
+        user node[:elasticsearch][:user]
+
+        mode 0770
+        recursive true
+        action :create
+      end
+
       if git_config.has_key?('ssh_key')
-        ssh_key_file = "#{node[:elasticsearch][:home_dir]}/#{name}_id_rsa"
+        # Use SSH key for connection to git
+        ssh_key_file = "#{plugin_directory}/id_rsa"
 
         file ssh_key_file do
           user node[:elasticsearch][:user]
-          mode "0600"
+          mode 0600
           content git_config['ssh_key']
         end
       end
 
-      file_location = "#{Chef::Config[:file_cache_path]}/#{name}"
+      template git_wrapper_file do
+        source "git_wrapper.sh.erb"
+        owner node[:elasticsearch][:user]
+
+        variables ssh_key_file: ssh_key_file
+        mode 0770
+      end
       
-      git file_location do
+      git plugin_source_directory do
         user node[:elasticsearch][:user]
+
         repository git_config['repository']
         revision git_config['revision']                   if git_config.has_key?('revision')
         enable_submodules git_config['enable_submodules'] if git_config.has_key?('enable_submodules')
-        ssh_wrapper "ssh -i #{ssh_key_file}"              if ssh_key_file
+
+        ssh_wrapper git_wrapper_file
 
         action :sync
       end
